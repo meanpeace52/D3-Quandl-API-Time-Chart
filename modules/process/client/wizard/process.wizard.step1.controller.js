f5bc89c4875dc246bd312a53a8261096ef4fee73
solar-dev23 <solar.dev023@gmail.com>
2016-11-14 08:32:43 +0200

    lab step 1 tweak

--- modules/process/client/wizard/process.wizard.step1.controller.js
+++ modules/process/client/wizard/process.wizard.step1.controller.js
@@ -6,6 +6,8 @@ angular.module('process')
         function ($state, $stateParams, Authentication, toastr, $log, ProcessStateService) {
             var vm = this;
 
+            vm.selectedoption = 'lab.process2.step2.existingmodel';
+
             vm.user = Authentication.user;
 
             ProcessStateService.saveProcessData({});
@@ -16,5 +18,10 @@ angular.module('process')
             ProcessStateService.setSelectedDataset({});
             ProcessStateService.saveTransformSteps([]);
             ProcessStateService.setState('lab.process2.step1');
+
+            vm.setSelectedOption = function(state){
+                ProcessStateService.changeState(state);
+                $state.go(state);
+            };
 
         }]);
